123039d380eba2130f458f05dcd4f98f89ac40d9
Kyle Staub <kylestaub3@gmail.com>
2019-05-02 19:28:53 -0600

    patch: formatting changes

--- {{cookiecutter.name}}/{{cookiecutter.task_filename}}.py
+++ {{cookiecutter.name}}/{{cookiecutter.task_filename}}.py
@@ -1,26 +1,26 @@
 {%- for task in cookiecutter.tasks %}
 from luigi import {{task.task_inherits}}
-{%- if task.parameters != "null" %}
-{%- for param in task.parameters %}
-{ % - if param.classpath != "null" %}
+    {%- if task.parameters != "null" %}
+        {%- for param in task.parameters %}
+            {%- if param.classpath != "null" %}
 from {{param.classpath}} import {{param.type}}
-{%- endif %}
-{%- endfor %}
-{%- endif %}
-{%- if task.requires != "null" %}
-{%- for req in task.requires %}
-{ % - if req.classpath != "null" %}
+            {%- endif %}
+        {%- endfor %}
+    {%- endif %}
+    {%- if task.requires != "null" %}
+        {%- for req in task.requires %}
+            {% - if req.classpath != "null" %}
 from {{req.classpath}} import {{req.name}}
-{%- endif %}
-{%- endfor %}
-{%- endif %}
-{%- if task.requires != "None" %}
-{%- for out in task.outputs %}
-{ % - if out.classpath != "null" %}
+            {%- endif %}
+        {%- endfor %}
+    {%- endif %}
+    {%- if task.requires != "None" %}
+        {%- for out in task.outputs %}
+            {%- if out.classpath != "null" %}
 from {{out.classpath}} import {{out.name}}
-{%- endif %}
-{%- endfor %}
-{%- endif %}
+            {%- endif %}
+        {%- endfor %}
+    {%- endif %}
 {%- endfor %}
 from pset_utils.luigi.task import *
 from pset_utils.luigi.output import *
@@ -31,16 +31,16 @@ class {{task.name}}({{task.inherits_from}}):
     __version__ = '1.0.0'
     _id = {{task.id}}
     {%- if task.parameters != "None" %}
-    {%- for param in task.parameters %}
+        {%- for param in task.parameters %}
     {{param.name}} = {{param.type}}()
-    {%- endfor %}
+        {%- endfor %}
     {%- endif %}
 
     {%- if task.requires != "None" %}
     requires = Requires()
-    {%- for req in task.requires %}
+        {%- for req in task.requires %}
     {{req.name.lower()}} = Requirement({{req.name}})
-    {%- endfor %}
+        {%- endfor %}
     {%- endif %}
 
     {%- if task.outputs.salted %}
