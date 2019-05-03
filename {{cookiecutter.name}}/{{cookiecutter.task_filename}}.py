e24d4fa7804d6a64040f75658c2da49305a63a95
Kyle Staub <kylestaub3@gmail.com>
2019-05-02 19:19:48 -0600

    feat: refactoring to fit Workflow model

---
+++ {{cookiecutter.name}}/{{cookiecutter.task_filename}}.py
@@ -0,0 +1,56 @@
+{%- for task in cookiecutter.tasks %}
+from luigi import {{task.task_inherits}}
+{%- if task.parameters != "null" %}
+{%- for param in task.parameters %}
+{ % - if param.classpath != "null" %}
+from {{param.classpath}} import {{param.type}}
+{%- endif %}
+{%- endfor %}
+{%- endif %}
+{%- if task.requires != "null" %}
+{%- for req in task.requires %}
+{ % - if req.classpath != "null" %}
+from {{req.classpath}} import {{req.name}}
+{%- endif %}
+{%- endfor %}
+{%- endif %}
+{%- if task.requires != "None" %}
+{%- for out in task.outputs %}
+{ % - if out.classpath != "null" %}
+from {{out.classpath}} import {{out.name}}
+{%- endif %}
+{%- endfor %}
+{%- endif %}
+{%- endfor %}
+from pset_utils.luigi.task import *
+from pset_utils.luigi.output import *
+{%- for task in cookiecutter.tasks %}
+
+
+class {{task.name}}({{task.inherits_from}}):
+    __version__ = '1.0.0'
+    _id = {{task.id}}
+    {%- if task.parameters != "None" %}
+    {%- for param in task.parameters %}
+    {{param.name}} = {{param.type}}()
+    {%- endfor %}
+    {%- endif %}
+
+    {%- if task.requires != "None" %}
+    requires = Requires()
+    {%- for req in task.requires %}
+    {{req.name.lower()}} = Requirement({{req.name}})
+    {%- endfor %}
+    {%- endif %}
+
+    {%- if task.outputs.salted %}
+    output = SaltedOutput(base_dir="data", target_class={{task.outputs.name}})
+    {%- else %}
+    output = TargetOutput(base_dir="data", target_class={{task.outputs.name}})
+    {%- endif %}
+
+    def run(self):
+        raise NotImplementedError
+{%- endfor %}
+
+
